01bdc98a0596a6d4e441983f83363403f461e294
thuan3598 <nguyenthuann002@gmail.com>
2023-12-12 09:47:21 +0700

    code

---
+++ Process_Termination_message/src/userprog/build/userprog/ipc.d
@@ -0,0 +1,4 @@
+userprog/ipc.o: ../../userprog/ipc.c ../../lib/kernel/list.h \
+ ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+ ../../lib/debug.h ../../threads/synch.h ../../userprog/ipc.h \
+ ../../threads/malloc.h
---
+++ Process_Termination_message/src/userprog/build/userprog/syscall.d
@@ -0,0 +1,7 @@
+userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
+ ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+ ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+ ../../lib/kernel/stdio.h ../../lib/syscall-nr.h ../../lib/string.h \
+ ../../threads/interrupt.h ../../threads/thread.h ../../lib/kernel/list.h \
+ ../../threads/vaddr.h ../../threads/loader.h ../../devices/shutdown.h \
+ ../../userprog/process.h ../../filesys/filesys.h ../../filesys/off_t.h
